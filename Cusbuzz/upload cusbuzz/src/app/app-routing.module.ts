cbafd33367db7456997242c21c3c49af7af27607
SuhasP121 <60957766+suhas1896@users.noreply.github.com>
2022-10-17 17:42:06 +0530

    cusbuzz

---
+++ Cusbuzz/upload cusbuzz/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from 'src/app/login/login.component'
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:"home",component:HomepageComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
